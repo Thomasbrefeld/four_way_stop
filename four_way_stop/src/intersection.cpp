83ea28eec3d4e335b8441dbe060d8dbb8e72d3f9
Thomasbrefeld <thomas.brefeld@gmail.com>
2021-12-07 16:59:10 -0500

    added ACTor2 support

---
+++ four_way_stop/src/intersection.cpp
@@ -0,0 +1,274 @@
+#include "intersection.h"
+
+Intersection::Intersection(ros::NodeHandle &node, ros::NodeHandle &priv_nh){
+    navSub = node.subscribe<sensor_msgs::NavSatFix>("/piksi/navsatfix_best_fix", 1, &Intersection::navCB, this);
+    scanSub = node.subscribe<sensor_msgs::LaserScan>("/scan", 1, &Intersection::scanCB, this);
+    gearSub = node.subscribe<dbw_polaris_msgs::GearCmd>("/vehicle/gear_cmd", 1, &Intersection::gearCB, this);
+    ulcSub = node.subscribe<dataspeed_ulc_msgs::UlcReport>("vehicle/ulc_report", 1, &Intersection::ulcCB, this);
+    steeringSub = node.subscribe<dbw_polaris_msgs::SteeringReport>("/vehicle/steering_report", 1, &Intersection::steeringCB, this);
+    waypointSub = node.subscribe<geometry_msgs::Twist>("/waypoint/cmd", 1, &Intersection::waypointCmdCB, this);
+
+    enablePub = node.advertise<std_msgs::Empty>("/vehicle/enable", 1);
+    twistPub = node.advertise<geometry_msgs::Twist>("/vehicle/cmd_vel", 1);
+    gearPub = node.advertise<dbw_polaris_msgs::GearCmd>("/vehicle/gear_cmd", 1);
+    waypointPub = node.advertise<sensor_msgs::NavSatFix>("/waypoint/waypoint", 1);
+
+    server.setCallback(boost::bind(&Intersection::dynamicReconfigureCB, this, _1, _2));
+
+    //variables
+    status = initiate;
+    rateSleep = .1;
+    speed = 2.5;
+    maxTurn = 1.3;
+    stopCount = 20;
+    changeAngle = 0;
+    turningAgnle = .1;
+    turnSpeedMulti = 1.2;
+    minForwardSpeed = .5;
+    reverseDistance = .00007;
+    waypointFoundDistance = .00003;
+    minLidarStopCount = 0;
+
+    //waypoints
+    sensor_msgs::NavSatFix tmp;
+    tmp.latitude = 42.472398067715865;
+    tmp.longitude = -83.25019544811526;
+    waypoints.push(tmp);
+    tmp.latitude = 42.47240204214436;
+    tmp.longitude = -83.24989903827807;
+    waypoints.push(tmp);
+    
+    //lidar box
+    maxRange = 10;
+    minRange = 2;
+    maxIndex = 30;
+    minIndex = -30;
+}
+
+void Intersection::dynamicReconfigureCB(four_way_stop::commanderConfig& msg, uint32_t level){
+    if (!msg.initiate)
+        status = initiate;
+    else
+        status = waypoint;
+    
+    speed = msg.speed;
+    rateSleep = msg.rateSleep;
+    waypointFoundDistance = msg.waypointFoundDistance;
+    maxTurn = msg.maxTurn;
+    turningAgnle = msg.turningAgnle;
+    turnSpeedMulti = msg.turnSpeedMulti;
+    minForwardSpeed = msg.minForwardSpeed;
+    reverseDistance = msg.reverseDistance;
+    stopCount = msg.stopCount;
+    minLidarStopCount = msg.minLidarStopCount;
+
+}
+
+double Intersection::calcHeading(sensor_msgs::NavSatFix nav1, sensor_msgs::NavSatFix nav2){
+    double dLon = (nav2.longitude-nav1.longitude);
+    double y = sin(dLon) * cos(nav2.latitude);
+    double x = cos(nav1.latitude)*sin(nav2.latitude) - sin(nav1.latitude)*cos(nav2.latitude)*cos(dLon);
+    double out = abs(atan2(y, x));
+    return out;
+}
+
+void Intersection::steeringCB(const dbw_polaris_msgs::SteeringReport::ConstPtr& msg){
+    if (steer.header.seq != 0)
+        lastSteer = steer;
+    else {
+        steer = *msg;
+        return;
+    }
+    steer = *msg;
+
+    double timeChange = (steer.header.stamp - lastSteer.header.stamp).toSec();
+    changeAngle += timeChange * (steer.steering_wheel_angle * ulcReport.speed_meas);
+    //ROS_INFO_STREAM(changeAngle << " - " << timeChange * (steer.steering_wheel_angle * ulcReport.speed_meas));
+}
+
+void Intersection::navCB(const sensor_msgs::NavSatFix::ConstPtr& msg){
+    if (nav.header.seq != 0)
+        lastNav = nav;
+    else {
+        nav = *msg;
+        return;
+    }
+    nav = *msg;
+
+    if (ulcReport.speed_meas > .1){
+        lastHeading = heading;
+
+        heading = calcHeading(nav, lastNav);
+    }
+}
+
+void Intersection::scanCB(const sensor_msgs::LaserScan::ConstPtr& msg){
+    int count = 0;
+    float range;
+
+    for (int index; index < msg->ranges.size(); index++){
+        float loc = (index * scan.angle_increment - M_PI) * 180 / M_PI;
+        float range = scan.ranges.at(index);
+        float inten = scan.intensities.at(index);
+
+        if (minIndex < loc && maxIndex > loc){
+            if (range > minRange && range < maxRange){
+                if (inten > 75){
+                    count++;
+                }
+            }
+        }
+    }
+
+    if (status == waypoint || status == lidarWait){
+        if (count > minLidarStopCount)
+            status = lidarWait;
+        else
+            status = waypoint;
+    }
+}
+
+double Intersection::distance(sensor_msgs::NavSatFix point1, sensor_msgs::NavSatFix point2){
+    return sqrt(pow(point1.latitude - point2.latitude, 2) + pow(point1.longitude - point2.longitude, 2));
+}
+
+void Intersection::stopFun(){
+    setCmd.linear.x = 0;
+    setCmd.angular.z = 0;
+    
+    while (ulcReport.speed_meas != 0){
+        ros::spinOnce();
+        twistPub.publish(setCmd);
+        ros::Duration(rateSleep).sleep();
+    }
+    for (int i = 0; i < stopCount; ++i){
+        ros::spinOnce();
+        twistPub.publish(setCmd);
+        ros::Duration(rateSleep).sleep();
+    }
+
+}
+
+void Intersection::run(){
+    ros::Duration(1).sleep();
+    enablePub.publish(std_msgs::Empty());
+    waypointPub.publish(waypoints.front());
+    ROS_INFO_STREAM("Four Way Stop Node Started!");
+
+    while (ros::ok()){
+        ros::spinOnce();
+
+        setCmd.linear.x = 0;
+        setCmd.linear.y = 0;
+        setCmd.linear.z = 0; 
+        setCmd.angular.x = 0;
+        setCmd.angular.y = 0;
+        setCmd.angular.z = 0;
+
+        if (status != initiate){
+            switch (status){
+                case waypoint:
+                    setGear.cmd.gear = dbw_polaris_msgs::Gear::DRIVE;
+                    gearPub.publish(setGear);
+                    if (distance(nav, waypoints.front()) < waypointFoundDistance){
+                        status = turn;
+                        waypoints.push(waypoints.front());
+                        waypoints.pop();
+                        waypointPub.publish(waypoints.front());
+                    }
+                    setCmd = waypointCmd;
+                    break;
+                case turn:
+                    switch (turnSequence){
+                        case 1:
+                            ROS_INFO_STREAM("TurnSequence " << turnSequence);
+                            changeAngle = 0;
+                            turnSequence++;
+                            ROS_INFO_STREAM("TurnSequence End" << turnSequence);
+                        case 2:
+                            ROS_INFO_STREAM("TurnSequence " << turnSequence);
+                            setCmd.linear.x = speed * turnSpeedMulti;
+                            setCmd.angular.z = maxTurn;
+
+                            ROS_INFO_STREAM("ChangeAngle: " << changeAngle << " ABS(GhangeAngle) " << abs(changeAngle) << " turning angle: " << turningAgnle);
+                            if (abs(changeAngle) < turningAgnle)
+                                break;
+
+
+                            distNav = nav;
+                            stopFun();
+                            setGear.cmd.gear = dbw_polaris_msgs::Gear::REVERSE;
+                            gearPub.publish(setGear);
+                            stopFun();
+                            turnSequence++;
+                            ROS_INFO_STREAM("TurnSequence End" << turnSequence);
+                        case 3: 
+                            ROS_INFO_STREAM("TurnSequence " << turnSequence);
+                            setCmd.linear.x = -speed * turnSpeedMulti;
+                            setCmd.angular.z = 0;
+                            changeAngle = 0;
+
+                            ROS_INFO_STREAM("ulcReport.speed_meas: " << ulcReport.speed_meas << " minForwardSpeed: " << -minForwardSpeed );
+                            if (ulcReport.speed_meas > -minForwardSpeed)
+                                break;
+
+                            turnSequence++;
+                            ROS_INFO_STREAM("TurnSequence End" << turnSequence);
+
+                        case 4:
+                            ROS_INFO_STREAM("TurnSequence " << turnSequence);
+                            setCmd.linear.x = -speed * .9;
+                            setCmd.angular.z = maxTurn/2;
+
+                            ROS_INFO_STREAM("ChangeAngle: " << changeAngle << " ABS(GhangeAngle) " << abs(changeAngle) << " turning angle: " << turningAgnle);
+                            if (abs(changeAngle) < turningAgnle * reverseDistance)
+                                break;
+                            //if (distance(nav, distNav) < reverseDistance)
+                            //    break;
+                            
+                            stopFun();
+                            setGear.cmd.gear = dbw_polaris_msgs::Gear::DRIVE;
+                            gearPub.publish(setGear);
+                            stopFun();
+                            turnSequence++;
+                            ROS_INFO_STREAM("TurnSequence End" << turnSequence);
+                        case 5:
+                            ROS_INFO_STREAM("TurnSequence " << turnSequence);
+                            setCmd.linear.x = speed;
+                            setCmd.angular.z = 0;
+
+                            if (ulcReport.speed_meas < minForwardSpeed)
+                                break;
+
+                            status = waypoint;
+                            ROS_INFO_STREAM("TurnSequence End" << turnSequence);
+                        default:
+                            turnSequence = 1;
+                    }
+                    break;
+                case lidarWait:
+                    setCmd.linear.x = 0;
+                    break;
+                default:
+                    ROS_INFO_STREAM("status: default");
+            }
+        }
+
+        ROS_INFO_STREAM(" status: " << status << " CMD: x: " << setCmd.linear.x << " z: " << setCmd.angular.z);
+        twistPub.publish(setCmd);
+        ros::Duration(rateSleep).sleep();
+    }
+}
+
+int main(int argc, char** argv){
+    ros::init(argc, argv, "four_way_stop_node");
+    ros::NodeHandle node;
+    ros::NodeHandle priv_nh("~");
+
+    Intersection intersection(node, priv_nh);
+    
+    intersection.run();
+}
+
+
+
---
+++ four_way_stop/src/intersection.h
@@ -0,0 +1,98 @@
+#ifndef INTERSECTION_H_
+#define INTERSECTION_H_
+
+#include <ros/ros.h>
+#include <std_msgs/Int8.h>
+#include <std_msgs/Empty.h>
+#include <sensor_msgs/NavSatFix.h>
+#include <sensor_msgs/LaserScan.h>
+#include <dbw_polaris_msgs/GearCmd.h>
+#include <dataspeed_ulc_msgs/UlcReport.h>
+#include <dbw_polaris_msgs/SteeringReport.h>
+#include <four_way_stop/commanderConfig.h>
+#include <dynamic_reconfigure/server.h>
+#include "geometry_msgs/Point.h"
+#include "geometry_msgs/Twist.h"
+
+#include <queue>
+
+class Intersection{
+public:
+    Intersection(ros::NodeHandle &node, ros::NodeHandle &priv_nh);
+    void run();
+private:
+    void navCB(const sensor_msgs::NavSatFix::ConstPtr&);
+    void scanCB(const sensor_msgs::LaserScan::ConstPtr&);
+    void gearCB(const dbw_polaris_msgs::GearCmd::ConstPtr& msg){gear = *msg;};
+    void ulcCB(const dataspeed_ulc_msgs::UlcReport::ConstPtr& msg){ulcReport = *msg;};
+    void steeringCB(const dbw_polaris_msgs::SteeringReport::ConstPtr&);
+    void waypointCmdCB(const geometry_msgs::Twist::ConstPtr& msg){waypointCmd = *msg;};
+    void dynamicReconfigureCB(four_way_stop::commanderConfig&, uint32_t);
+
+    double distance(sensor_msgs::NavSatFix, sensor_msgs::NavSatFix);
+    double calcHeading(sensor_msgs::NavSatFix, sensor_msgs::NavSatFix);
+    void stopFun();
+
+    dynamic_reconfigure::Server<four_way_stop::commanderConfig> server;
+
+    ros::NodeHandle nh;
+
+    ros::Subscriber navSub;
+    ros::Subscriber scanSub;
+    ros::Subscriber gearSub;
+    ros::Subscriber ulcSub;
+    ros::Subscriber steeringSub;
+    ros::Subscriber waypointSub;
+
+    ros::Publisher enablePub;
+    ros::Publisher disablePub;
+    ros::Publisher twistPub;
+    ros::Publisher gearPub;
+    ros::Publisher waypointPub;
+
+    sensor_msgs::NavSatFix nav;
+    sensor_msgs::NavSatFix lastNav;
+    sensor_msgs::NavSatFix distNav;
+    sensor_msgs::LaserScan scan;
+    dbw_polaris_msgs::GearCmd gear;
+    dbw_polaris_msgs::SteeringReport lastSteer;
+    dbw_polaris_msgs::SteeringReport steer;
+    dataspeed_ulc_msgs::UlcReport ulcReport;
+    geometry_msgs::Twist waypointCmd;
+
+    geometry_msgs::Twist setCmd;
+    dbw_polaris_msgs::GearCmd setGear;
+
+    enum Status {initiate, stop, waypoint, lidarWait, turn};
+    Status status;
+
+    std::queue<sensor_msgs::NavSatFix> waypoints;
+
+    int turnSequence;
+    int sleepCount;
+
+    double heading;
+    double lastHeading;
+    double changeAngle;
+
+    //Dynamic Reconfigure Variables
+    double speed;
+    double maxTurn;
+    double rateSleep;
+    double waypointFoundDistance;
+    double turningAgnle;
+    double turnSpeedMulti;
+    double minForwardSpeed;
+    double reverseDistance;
+    int stopCount;
+    int minLidarStopCount;
+
+    double maxRange;
+    double minRange;
+    double maxIndex;
+    double minIndex;
+
+    int timeTmp;
+};
+
+#endif
